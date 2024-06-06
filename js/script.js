b2cf059d5cbd3dffc35c01196ba7832211035911
ViniciusBitencourt98 <viniciusbitencourt1998@outlook.com>
2024-06-06 18:19:29 -0300

    Feito maskara de cep, telefone e melhorado função da API.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-    async function buscaCEP(cep) {
-      if (cep.length !== 8) {
-        document.getElementById('estado').value = '';
-        document.getElementById('cidade').value = '';
-        document.getElementById('endereco').value = '';
-        document.getElementById('bairro').value = '';
-          return;
-      }
-  
-      try {
-          const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-          if (!response.ok) {
-              throw new Error('Erro ao buscar o CEP');
-          }
-  
-          const data = await response.json();
-          if (data.erro) {
-              alert('CEP não encontrado');
-              return;
-          }
-          
-          document.getElementById('estado').value = data.uf;
-          document.getElementById('cidade').value = data.localidade;
-          document.getElementById('endereco').value = data.logradouro;
-          document.getElementById('bairro').value = data.bairro;
-      } catch (error) {
-          console.error(error);
-          alert('Erro ao buscar o CEP');
-      }
-  }
-  
+$(document).ready(function() {
+    $('#cep').mask('00000-000');
+    $('#tel-1').mask('(00) 00000-0000');
+    $('#tel-2').mask('(00) 00000-0000');
+    
+    document.getElementById('cep').addEventListener('input', function () {
+        const cep = this.value.replace(/\D/g, ''); // Remove non-digit characters
+        if (cep.length < 8) {
+            document.getElementById('estado').value = '';
+            document.getElementById('cidade').value = '';
+            document.getElementById('endereco').value = '';
+            document.getElementById('bairro').value = '';
+        } else if (cep.length === 8) {
+            buscaCEP(cep);
+        }
+    });
+});
+
+async function buscaCEP(cep) {
+    if (cep.length === 8){
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar o CEP');
+        } 
+
+        const data = await response.json();
+        if (data.erro) {
+            alert('CEP não encontrado');
+            return;
+        }
+
+        document.getElementById('estado').value = data.uf;
+        document.getElementById('cidade').value = data.localidade;
+        document.getElementById('endereco').value = data.logradouro;
+        document.getElementById('bairro').value = data.bairro;
+    } catch (error) {
+        console.error(error);
+        alert('Erro ao buscar o CEP');
+    } }
+}
