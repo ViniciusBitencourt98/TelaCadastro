dc5ee7830c0fbacc9c324d23dc7cb5e7ffaadbf5
ViniciusBitencourt98 <viniciusbitencourt1998@outlook.com>
2024-11-21 18:57:46 -0300

    Iniciando codding da tela de login

---
+++ login.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="css/style.css">
+<div class="container-login">
+    <div class="container-nome">
+        <div class="container-nome-principal">SDC</div>
+        <div class="container-nome-secundario">Sistema de cadastro de pessoas</div>
+    </div>
+    <div class="container-login">bbbb</div>
+</div>
