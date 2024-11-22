6c9fd27b08d764561e5fac01326cdfd056873356
ViniciusBitencourt98 <viniciusbitencourt1998@outlook.com>
2024-11-22 18:56:26 -0300

    Iniciando codding da tela de login

--- login.php
+++ login.php
@@ -1,8 +1,15 @@
 <link rel="stylesheet" href="css/style.css">
 <div class="container-login">
     <div class="container-nome">
-        <div class="container-nome-principal">SDC</div>
+        <div class="container-nome-principal">SDCP</div>
         <div class="container-nome-secundario">Sistema de cadastro de pessoas</div>
     </div>
-    <div class="container-login">bbbb</div>
+    <div class="container-login-input">
+        <div class="container-login-gif"></div>
+        <div class="container-bemvindo">
+            <div class="container-nome-bemvindo">Bem vindo !</div>
+            <div>SDCP - Sistema de cadastro de pessoas</div>
+        </div>
+        <div>c</div>
+    </div>
 </div>
